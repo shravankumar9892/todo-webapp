d450f9157f3bc0918d4398b3d20de7049ac4ac53
shravankumar9892 <shravankumarshetty9892@gmail.com>
2020-04-25 07:24:26 +0530

    basic app done

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  todoArray = []
+  addTodo(value):any{    
+    this.todoArray.push(value)    
+    console.log(this.todoArray)  
+  } 
+}
